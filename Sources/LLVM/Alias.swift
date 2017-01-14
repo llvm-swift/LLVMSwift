545949bf35ca3236a0be2db4d5ceaaf177035fd8
Robert Widmann <devteam.codafi@gmail.com>
2017-01-13 22:56:26 -0700

    Add linkage and visibility accessors to Alias

--- Sources/LLVM/Alias.swift
+++ Sources/LLVM/Alias.swift
@@ -5,6 +5,18 @@ import cllvm
 public struct Alias: IRValue {
   internal let llvm: LLVMValueRef
 
+  /// Retrieves the linkage information for this alias.
+  public var linkage: Linkage {
+    get { return Linkage(llvm: LLVMGetLinkage(asLLVM())) }
+    set { LLVMSetLinkage(asLLVM(), newValue.llvm) }
+  }
+
+  /// Retrieves the visibility style for this alias.
+  public var visibility: Visibility {
+    get { return Visibility(llvm: LLVMGetVisibility(asLLVM())) }
+    set { LLVMSetVisibility(asLLVM(), newValue.llvm) }
+  }
+
   /// Retrieves the underlying LLVM value object.
   public func asLLVM() -> LLVMValueRef {
     return llvm
