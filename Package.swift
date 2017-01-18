b3b0337188ec6a5f3e199639ca51af113ffd9a3f
Harlan <harlan@harlanhaskins.com>
2017-01-17 23:51:53 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "LLVM",
   dependencies: [
-    .Package(url: "https://github.com/harlanhaskins/cllvm.git", majorVersion: 0),
+    .Package(url: "https://github.com/trill-lang/cllvm.git", majorVersion: 0),
   ]
 )
