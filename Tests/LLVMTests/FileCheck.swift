768acf72b82c246b5ad3ccfd85a1246e0394bfb0
Harlan Haskins <harlan@harlanhaskins.com>
2017-02-07 23:03:09 -0500

    Added ANSIColor and ColoredANSIStream for printing colored diagnostic messages

--- Tests/LLVMTests/FileCheck.swift
+++ Tests/LLVMTests/FileCheck.swift
@@ -1224,16 +1224,34 @@ private struct CheckString {
   }
 }
 
-private enum DiagnosticKind {
+private enum DiagnosticKind: String {
   case error
   case warning
   case note
+
+  var color: ANSIColor {
+    switch self {
+    case .error: return .red
+    case .warning: return .magenta
+    case .note: return .green
+    }
+  }
 }
 
+struct StdOutStream: TextOutputStream {
+    mutating func write(_ string: String) {
+        print(string, terminator: "")
+    }
+}
+
+var stdoutStream = StdOutStream()
+var diagnosticStream = ColoredANSIStream(&stdoutStream)
+
 private func diagnose(_ kind : DiagnosticKind, _ loc : CheckLoc, _ message : String) {
-  print(message)
+  diagnosticStream.write("\(kind): ", with: [.bold, kind.color])
+  diagnosticStream.write("\(message)\n", with: [.bold])
   let msg = loc.message
   if !msg.isEmpty {
-    print(msg)
+    diagnosticStream.write("\(msg)\n")
   }
 }
