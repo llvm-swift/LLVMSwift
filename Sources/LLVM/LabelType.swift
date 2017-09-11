7507ee546518de6d77f4001bd9cc26349b42f023
vdka <ethanjackwitz@gmail.com>
2017-09-11 14:25:34 +1000

    Added context to LabelType

--- Sources/LLVM/LabelType.swift
+++ Sources/LLVM/LabelType.swift
@@ -4,11 +4,23 @@ import cllvm
 
 /// `LabelType` represents code labels.
 public struct LabelType: IRType {
+
+  /// Returns the context associated with this module.
+  public let context: Context?
+
   /// Creates a code label.
-  public init() {}
+  ///
+  /// - parameter context: The context to create this type in
+  /// - SeeAlso: http://llvm.org/docs/ProgrammersManual.html#achieving-isolation-with-llvmcontext
+  public init(context: Context? = nil) {
+      self.context = context
+  }
 
   /// Retrieves the underlying LLVM type object.
   public func asLLVM() -> LLVMTypeRef {
+    if let context = context {
+       return LLVMLabelTypeInContext(context.llvm)
+    }
     return LLVMLabelType()
   }
 }
