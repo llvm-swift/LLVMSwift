c3a12b41ed114b66e75373a8dac6779597b1aa7a
Robert Widmann <devteam.codafi@gmail.com>
2017-10-02 21:44:02 -0400

    Disable and rewrite Metadata and Token type inits
    These initializers do not have corresponding create
    calls in LLVM yet and so cannot possibly be used from
    user code.  Disable them with a message.

--- Sources/LLVM/TokenType.swift
+++ Sources/LLVM/TokenType.swift
@@ -6,13 +6,19 @@ import cllvm
 /// uses of the value must not attempt to introspect or obscure it. As such, it
 /// is not appropriate to have a `PHI` or `select` of type `TokenType`.
 public struct TokenType: IRType {
-  internal let llvm: LLVMTypeRef
+  /// Returns the context associated with this type.
+  public let context: Context
 
   /// Initializes a token type from the given LLVM type object.
-  public init(llvm: LLVMTypeRef) { self.llvm = llvm }
+  ///
+  /// - parameter context: The context to create this type in
+  /// - SeeAlso: http://llvm.org/docs/ProgrammersManual.html#achieving-isolation-with-llvmcontext
+  public init(in context: Context = Context.global) {
+    self.context = context
+  }
 
   /// Retrieves the underlying LLVM type object.
   public func asLLVM() -> LLVMTypeRef {
-    return llvm
+    fatalError("This version of LLVM does not support the creation of TokenType objects")
   }
 }
