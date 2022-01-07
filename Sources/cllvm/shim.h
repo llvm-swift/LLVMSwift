163a5960e8da95308fe50d203279fe6d9bc604dd
Max Desiatov <max@desiatov.com>
2022-01-07 11:55:04 +0000

    Update `cllvm/shim.h`

--- Sources/cllvm/shim.h
+++ Sources/cllvm/shim.h
@@ -17,7 +17,7 @@
 #include <llvm-c/Linker.h>
 #include <llvm-c/lto.h>
 #include <llvm-c/Object.h>
-#include <llvm-c/Orc.h>
+#include <llvm-c/LLJIT.h>
 #include <llvm-c/Support.h>
 #include <llvm-c/Target.h>
 #include <llvm-c/TargetMachine.h>
